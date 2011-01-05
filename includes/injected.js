322d076d2bd418e8f2a42bbc2ca65602cbc6a5e2
karl <karl+github@la-grange.net>
2011-01-05 14:29:48 -0500

    Version 0.7 - see the README file for the list of changes. Starting now, the changes will be under versioning

---
+++ includes/injected.js
@@ -0,0 +1,40 @@
+// This code is under Apache 2.0 License
+// Check the README.mdown
+
+window.addEventListener('load', function() {
+
+    String.prototype.startsWith = function(prefix) {
+        return this.indexOf(prefix) === 0;
+    }
+    
+    // Get all "blockquote" and "q" elements in a single array.
+    var quotes = document.querySelectorAll('blockquote, q');
+    var cite, a, p, isbn;
+    var linkstyle = "font-size:80%;font-weight:bold;text-decoration:none;margin:0;padding:0;";
+    
+    // Loop through the quotes array and try to get the "cite" attribute.
+    for (var i = 0, quote; quote = quotes[i]; i++) {
+        cite = quote.cite;
+        
+        // If there's a "cite" attribute, add a "read more" link
+        if (cite !== '') {
+            // if the cite attribute starts with urn:isbn: 
+            // we link to openlibrary http://openlibrary.org/isbn/123
+            
+            if (cite.startsWith("urn:isbn:")) {
+                isbn = cite.substring(9).replace("-","");
+                cite = "http://openlibrary.org/isbn/" + isbn;
+            }
+            a = document.createElement('a');
+            a.setAttribute('href', cite);
+            a.innerHTML = 'ⓘ';
+            span = document.createElement('span');
+            span.className = 'quotesource';
+            a.style = linkstyle;
+            span.appendChild(a);
+            quote.appendChild(span);
+        } 
+    }
+
+}, false);
+
