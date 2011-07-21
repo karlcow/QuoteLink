06711018dd26046abfee680600201066dacbe250
karl <karl+github@la-grange.net>
2011-07-21 18:00:24 -0400

    Adding CSS to be less disruptive with the initial design of the page

--- includes/injected.js
+++ includes/injected.js
@@ -9,8 +9,9 @@ window.addEventListener('load', function() {
     
     // Get all "blockquote" and "q" elements in a single array.
     var quotes = document.querySelectorAll('blockquote, q');
-    var cite, a, span, isbn;
-    var linkstyle = "font-size:80%;font-weight:bold;text-decoration:none;margin:0;padding:0;";
+    var cite, a, div, isbn;
+    var linkstyle = "position:relative;font-size:80%;font-weight:bold;text-decoration:none;margin:0;padding:0;";
+    var sourcestyle = 
     
     // Loop through the quotes array and try to get the "cite" attribute.
     for (var i = 0, quote; quote = quotes[i]; i++) {
@@ -28,14 +29,33 @@ window.addEventListener('load', function() {
             a = document.createElement('a');
             a.setAttribute('href', cite);
             a.innerHTML = 'ⓘ';
-            span = document.createElement('span');
-            span.className = 'quotesource';
-            a.style = linkstyle;
-            a.title = "Source at " + cite
-            span.appendChild(a);
-            quote.appendChild(span);
+            a.style.textDecoration = "none";
+            a.style.color = "#333";
+            a.style.opacity = "0.75";
+            a.title = "Source at " + qcite
+            quote.style.position = "relative";
+            // wrapping it in a div
+            div = document.createElement('div');
+            div.className = 'quotesource';
+            div.style.position = "absolute";
+            div.style.margin = "0";
+            div.style.padding = "0";
+            // two cases: BLOCKQUOTE OR QUOTE
+            if (quote.tagName == 'BLOCKQUOTE') {
+                div.style.right = "5px";
+                div.style.top = "5px";
+                div.style.fontSize = "1.5em";
+                div.style.fontWeight = "800";
+            } else {
+                div.style.display = "inline";
+                div.style.right = "5px";
+                div.style.top = "-10px";
+                div.style.fontSize = ".8em";
+            }
+
+            div.appendChild(a);
+            quote.appendChild(div);
         } 
     }
 
 }, false);
-
